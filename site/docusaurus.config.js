0a3c4d07c38eb6cfdddc9dcfac7bbdca8746604e
Adam Pugh <adamthepugh@gmail.com>
2026-03-16 05:56:06 -0500

    Fix deprecated markdown link config

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -15,7 +15,12 @@ const config = {
   projectName: 'adamthepugh',
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   i18n: {
     defaultLocale: 'en',
